e32446a2a284b3faf6b0f3f11621c3feca158d2e
caiguangzheng <caiguangzheng@terminus.io>
2020-04-07 10:21:02 +0800

    update

--- rocketmq-demo/src/main/java/me/cai/demo/rocketmq/consumer/OrderConsumerDemo2.java
+++ rocketmq-demo/src/main/java/me/cai/demo/rocketmq/consumer/OrderConsumerDemo2.java
@@ -8,6 +8,7 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * me.cai.demo.rocketmq.consumer
@@ -27,7 +28,7 @@ public class OrderConsumerDemo2 implements RocketMQListener<MessageExt> {
     @Override
     public void onMessage(MessageExt messageExt) {
         log.debug("OrderConsumerDemo receive msgID:{}", messageExt.getMsgId());
-        String payload = new String(messageExt.getBody(), Charset.forName("utf-8"));
+        String payload = new String(messageExt.getBody(), StandardCharsets.UTF_8);
         log.debug("OrderConsumerDemo receive message:{}", payload);
     }
 }
--- rocketmq-demo/src/main/java/me/cai/demo/rocketmq/consumer/SimpleConsumerDemo.java
+++ rocketmq-demo/src/main/java/me/cai/demo/rocketmq/consumer/SimpleConsumerDemo.java
@@ -7,6 +7,7 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * me.cai.demo.rocketmq.consumer
@@ -24,7 +25,7 @@ public class SimpleConsumerDemo implements RocketMQListener<MessageExt> {
     @Override
     public void onMessage(MessageExt messageExt) {
         log.debug("SimpleConsumerDemo receive msgID:{}", messageExt.getMsgId());
-        String payload = new String(messageExt.getBody(), Charset.forName("utf-8"));
+        String payload = new String(messageExt.getBody(), StandardCharsets.UTF_8);
         log.debug("SimpleConsumerDemo receive message:{}", payload);
     }
 }
