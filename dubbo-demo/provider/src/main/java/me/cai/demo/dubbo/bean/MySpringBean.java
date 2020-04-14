53fcdac637581aeb2225a7c26cdf1645a9501845
caiguangzheng <caiguangzheng@terminus.io>
2020-04-14 16:15:32 +0800

    update

---
+++ dubbo-demo/provider/src/main/java/me/cai/demo/dubbo/bean/MySpringBean.java
@@ -0,0 +1,20 @@
+package me.cai.demo.dubbo.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * me.cai.demo.dubbo.bean
+ *
+ * @author caiguangzheng
+ * date: 2020/4/14
+ * mail: caiguangzheng@terminus.io
+ * description:
+ */
+@Component
+public class MySpringBean {
+
+    @Override
+    public String toString() {
+        return "MySpringBean.toString";
+    }
+}
--- dubbo-demo/provider/src/main/java/me/cai/demo/dubbo/provider/impl/ProviderDemoImpl.java
+++ dubbo-demo/provider/src/main/java/me/cai/demo/dubbo/provider/impl/ProviderDemoImpl.java
@@ -1,7 +1,10 @@
 package me.cai.demo.dubbo.provider.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import me.cai.demo.dubbo.bean.MySpringBean;
 import me.cai.demo.dubbo.provider.ProviderDemo;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
@@ -12,11 +15,16 @@ import org.apache.dubbo.config.annotation.Service;
  * mail: caiguangzheng@terminus.io
  * description:
  */
+@Slf4j
 @Service(timeout = 1000, retries = 0)
 public class ProviderDemoImpl implements ProviderDemo {
 
+    @Autowired
+    private MySpringBean mySpringBean;
+
     @Override
     public String sayHello() {
+        log.error(mySpringBean.toString());
         return "Hello";
     }
 
